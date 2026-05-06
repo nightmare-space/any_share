454880c894e86e94730e1b8613c24dcfbb3ee265
Laurie <mengyanshou@gmail.com>
2026-05-06 10:38:19 +0800

    Rename config to common

---
+++ lib/common/assets.dart
@@ -0,0 +1,19 @@
+import 'package:speed_share/common/config.dart';
+
+class Assets {
+  // 文件夹的svg资源key
+  static String dir = '${Config.flutterPackage}assets/icon/directory.svg';
+  // 文件的svg资源key
+  static String file = '${Config.flutterPackage}assets/icon/file.svg';
+}
+
+class SvgAssets {
+  // 文件夹的svg资源key
+  static String dir = 'assets/icon/v2/folder.svg';
+  // 文件的svg资源key
+  static String file = 'assets/icon/v2/file.svg';
+  // 消息的svg资源key
+  static String message = 'assets/icon/v2/message.svg';
+  // 设置的svg资源key
+  static String setting = 'assets/icon/v2/setting.svg';
+}
--- lib/config/assets.dart
+++
@@ -1,8 +0,0 @@
-import 'package:speed_share/config/config.dart';
-
-class Assets {
-  // 文件夹的svg资源key
-  static String dir = '${Config.flutterPackage}assets/icon/directory.svg';
-  // 文件的svg资源key
-  static String file = '${Config.flutterPackage}assets/icon/file.svg';
-}
